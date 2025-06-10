55b21c1803b1215bfda69fbfd97bbbcb2a07ba13
Jacob Ilagan <jilagan5@myseneca.ca>
2025-06-09 22:17:05 -0400

    Adding methods to the class

--- models/UserProfile.js
+++ models/UserProfile.js
@@ -1,6 +1,27 @@
 class User {
-  constructor(firstName, lastName) {
+  constructor(firstName, lastName, email) {
+    this.created = Date.now();
+    this.updated = Date.now();
     this.firstName = firstName;
     this.lastName = lastName;
+    this.email = email;
+    this.booksRead = [];
+    this.favouriteGenre = ['Nothing yet'];
+  }
+
+  get fullName() {
+    return this.getFullName();
+  }
+
+  getFullName() {
+    return `The user's full name is: ${this.firstName} ${this.lastName}.`;
+  }
+
+  get userEmail() {
+    return this.email;
+  }
+
+  analyzeUser() {
+    return `${this.getFullName()}'s favourite genre is: ${this.favouriteGenre[0]}`;
   }
 }
