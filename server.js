92a34728168c817a8a679905cc89ed859851e8ab
Jacob Ilagan <jilagan5@myseneca.ca>
2025-06-09 21:15:38 -0400

    Rename to server.js

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Root working!');
+  res.send('The root route is working!');
 });
 
 app.listen(port, () => {
-  console.log(`PageTurn listening on port: ${port}`);
+  console.log(`Server running on port ${port}`)
 });
+
