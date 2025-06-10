58aa3debf97b057b0871603abe51836b09d4f130
Jacob Ilagan <jilagan5@myseneca.ca>
2025-06-09 21:06:24 -0400

    Apply prettier

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Root working!');
+  res.send('Root working!');
 });
 
 app.listen(port, () => {
-    console.log(`PageTurn listening on port: ${port}`);
+  console.log(`PageTurn listening on port: ${port}`);
 });
